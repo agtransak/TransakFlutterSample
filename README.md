d6046eaa818a3a7a49a20538c30e42c2e2c7bac8
Anshul Garg <anshul.garg@transak.com>
2024-09-16 16:33:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# transak_flutter_sample
+
+A new Flutter project to showcase Transak widget implementation using webview_flutter package. 
---
+++ android/app/src/main/kotlin/com/transak/sample/transak_flutter_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.transak.sample.transak_flutter_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
